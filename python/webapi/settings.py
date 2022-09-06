588e6eae57ee21ac9215bde7adad84b7deb3612d
diegobritto <diegodesouzabritto@gmail.com>
2022-09-06 00:18:03 -0300

    renomeada pasta do projeto

--- python/webapi/settings.py
+++ python/webapi/settings.py
@@ -123,9 +123,6 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':10,
-
-
-
 }
 
 # Default primary key field type
